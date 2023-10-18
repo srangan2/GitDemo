279dbd9f6b98cceb19d7dc500d0aed0543ac6325
Srinivasa Rangan <srinivasa.rangan.hs@gmail.com>
2023-10-18 12:20:28 +0530

    Second commit

---
+++ coreJavaTraining/src/coreJavaTraining/NewGitProject.java
@@ -0,0 +1,12 @@
+package coreJavaTraining;
+
+public class NewGitProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is a git project to be committed");
+	}
+
+}
