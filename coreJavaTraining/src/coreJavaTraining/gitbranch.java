96aacbb5e9d9f2f8df183ba445097d8b16476309
Srinivasa Rangan <srinivasa.rangan.hs@gmail.com>
2023-10-18 12:59:39 +0530

    First Develop commit

---
+++ coreJavaTraining/src/coreJavaTraining/gitbranch.java
@@ -0,0 +1,11 @@
+package coreJavaTraining;
+
+public class gitbranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Develop branch is pushed now");
+	}
+
+}
