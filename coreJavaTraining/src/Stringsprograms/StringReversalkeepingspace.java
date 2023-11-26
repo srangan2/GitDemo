ffa128278406501810be461e6b3b081d014c68bd
srangan2 <srinivasa.rangan.hs@gmail.com>
2023-11-26 14:22:52 +0530

    Devops version1

--- coreJavaTraining/src/Stringsprograms/StringReversalkeepingspace.java
+++ coreJavaTraining/src/Stringsprograms/StringReversalkeepingspace.java
@@ -3,16 +3,31 @@ package Stringsprograms;
 import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 public class StringReversalkeepingspace {
+	
+	static String s = "Rahul Shetty Acadaemy";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s = "Rahul Shetty Academy";
+		int length = s.length();
+		char[] chArr = s.toCharArray();
 		
+		for(int i=chArr.length-1;i>=0;i--)
+		{
+			System.out.print(chArr[i]);
+			
+		}
+		System.out.println("");
+		
+		
+	
 		for(int i=s.length()-1;i>=0;i--)
 		{
 			System.out.print(s.charAt(i));
 		}
+		
+
+		
 		
 	}
 
